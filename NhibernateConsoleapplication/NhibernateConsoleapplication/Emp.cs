18147579afedc63e3dd2df768f5e9dada3a5e62a
BOBJIG <sarathchandra.7321@gmail.com>
2021-06-24 07:55:33 +0530

    Add files via upload
    Test NHibernate example

---
+++ NhibernateConsoleapplication/NhibernateConsoleapplication/Emp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhibernateConsoleapplication
+{
+    class Emp
+    {
+        public virtual int Emp_id { get; set; }
+        public virtual string Empname { get; set; }
+        public virtual Fulladress FullAdress  { get; set; }
+        public virtual ISet<Orders> OrderInformation { get; set; }
+    }
+}
---
+++ NhibernateConsoleapplication/NhibernateConsoleapplication/Employ.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhibernateConsoleapplication
+{
+    class Employ
+    {
+        public virtual int Employ_id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Designation { set; get; }
+    }
+}
