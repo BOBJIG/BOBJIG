a20c1d35be3992adca81e220a293181d0d01b525
BOBJIG <sarathchandra.7321@gmail.com>
2021-06-24 08:02:54 +0530

    Add files via upload
    sample aspx page functionality

---
+++ SampleAspxPage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SampleAspxPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
